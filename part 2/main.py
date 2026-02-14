f8a1132d36b2b656baa933ba8d7c2d89b727f987
salman_ghannam1 <sloome222@gmail.com>
2026-02-14 17:49:56 +0300

    adding main.py to test classes before adding api

--- part 2/main.py
+++ part 2/main.py
@@ -2,11 +2,16 @@ from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 
 def main():
+    print("MAIN STARTED ✅")
+
     repo = InMemoryRepository()
     User.set_repository(repo)
 
-    u1 = User.create("Salman", "Al-Mutairi", "salman@email.com", "1234")
-    print(u1.to_dict())
+    u1 = User.create("Salman", "Al-Mutairi", "s@email.com", "1234")
+    u2 = User.create("salwa","fares","sss@gmail.com.com",1234)
+    print("Created:", u1.to_dict())
+
+    print("All users:", [u.to_dict() for u in User.get_all()])
 
 if __name__ == "__main__":
     main()
