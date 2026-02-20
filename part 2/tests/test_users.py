9e5586a721907bbc62bff50058609bc0ad05149f
iihaad9 <fahadfawaz5@gmail.com>
2026-02-20 19:57:10 +0300

    Create test_users.py

---
+++ part 2/tests/test_users.py
@@ -0,0 +1,23 @@
+import unittest
+from app import create_app
+
+class TestUserEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+        self.app.testing = True
+
+    def test_create_user_success(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "Test",
+            "last_name": "User",
+            "email": "test_user@example.com"
+        })
+
+        self.assertEqual(response.status_code, 201)
+        data = response.get_json()
+        self.assertIn("id", data)
+
+if __name__ == "__main__":
+    unittest.main()
