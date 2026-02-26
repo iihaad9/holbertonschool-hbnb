25001d00d012aee3d95095a66498c16a2fc6dab1
Hanin157 <alshaneen856@gmail.com>
2026-02-26 11:52:10 +0300

    Fix string quotes and add namespace registration

--- part 2/app/__init__.py
+++ part 2/app/__init__.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Flask application factory"""
+
 from flask import Flask
 from flask_restx import Api
 
@@ -8,19 +11,21 @@ from app.api.v1.amenities import api as amenities_ns
 
 
 def create_app():
+    """Create and configure the Flask application"""
     app = Flask(__name__)
 
     api = Api(
         app,
-        version='1.0',
-        title='HBnB API',
-        description='HBnB Application API',
-        doc='/api/v1/'
+        version="1.0",
+        title="HBnB API",
+        description="HBnB Application API",
+        doc="/api/v1/"
     )
 
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    # Register namespaces
+    api.add_namespace(users_ns, path="/api/v1/users")
+    api.add_namespace(places_ns, path="/api/v1/places")
+    api.add_namespace(reviews_ns, path="/api/v1/reviews")
+    api.add_namespace(amenities_ns, path="/api/v1/amenities")
 
     return app
